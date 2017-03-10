ed71a2ea3ec390e808d345656e401febeed87d7c
Kosta Harlan <kosta@fastmail.com>
2017-03-10 14:15:48 -0500

    Issue #2785: Add RUM tracking script

--- init.rb
+++ init.rb
@@ -1,8 +1,10 @@
+require_dependency 'redmine_savas_customizations/hooks'
+
 Redmine::Plugin.register :redmine_savas_customizations do
   name 'Redmine Savas Customizations'
   author 'Kosta Harlan'
   description 'Savas Labs customizations'
-  version '0.1.0'
+  version '0.2.0'
   url 'https://github.com/savaslabs/redmine-savas-customizations'
   author_url 'http://savaslabs.com'
 end
